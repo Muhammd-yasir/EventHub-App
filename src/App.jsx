b2d907d7e9d49bdcefcd0e403f451eb9f8f2b0a6
Muhammad Yasir <anwarmuhammadyasir11@gmail.com>
2025-12-04 17:51:08 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { ThemeProvider } from "./context/ThemeContext";
-import { EventProvider } from "./context/EventContext";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
@@ -7,20 +5,16 @@ import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    <ThemeProvider>
-      <EventProvider>
-        <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
-          <Header />
-          <div className="flex flex-1">
-            <Sidebar />
-            <main className="flex-1 p-6">
-              <AppRoutes />
-            </main>
-          </div>
-          <Footer />
-        </div>
-      </EventProvider>
-    </ThemeProvider>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          <AppRoutes />
+        </main>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
