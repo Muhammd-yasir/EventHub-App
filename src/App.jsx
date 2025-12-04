d6c8a798563e4b59d8d6f27621fe4085746bd89e
Muhammad Yasir <anwarmuhammadyasir11@gmail.com>
2025-12-04 13:29:53 +0500

    Header, Footer, Sidebar, EventCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-
-import AppRoutes from "./AppRoutes"
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import AppRoutes from "./AppRoutes";
 
 function App() {
-
   return (
-    <>
-      <AppRoutes/>
-    </>
-  )
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          <AppRoutes />
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,20 +1,19 @@
-
 import { Routes, Route } from "react-router-dom";
-
-// Pages
 import Dashboard from "./pages/Dashboard";
-// import EventList from "./pages/EventList";
-// import CalendarView from "./pages/CalendarView";
-// import Profile from "./pages/Profile";
+import EventList from "./pages/EventList";
+import CalendarView from "./pages/CalendarView";
+import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
 const AppRoutes = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/events" element={<EventList />} />
+      <Route path="/calendar" element={<CalendarView />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
